34496e276b1ba804e629ecfc69cbef70e7c0574e
Julian <86843980+Juliandreslopez@users.noreply.github.com>
2023-02-03 10:00:34 -0800

    Update terraform_pc/simple_instance/jlopezs3.tf
    Co-authored-by: prisma-cloud-devsecops[bot] <89982750+prisma-cloud-devsecops[bot]@users.noreply.github.com>

--- terraform_pc/simple_instance/jlopezs3.tf
+++ terraform_pc/simple_instance/jlopezs3.tf
@@ -17,6 +17,17 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "jlopezs3" {
 
   rule {
     apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "jlopezs3" {
+  bucket = aws_s3_bucket.jlopezs3.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
       sse_algorithm     = "AES256"
     }
   }
