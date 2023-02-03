cf336875020d2d1cc6ef8761ea5fcc072ca44e4b
Julian <86843980+Juliandreslopez@users.noreply.github.com>
2023-02-03 10:26:01 -0800

    Update terraform_pc/simple_instance/jlopezs3.tf
    Co-authored-by: prisma-cloud-devsecops[bot] <89982750+prisma-cloud-devsecops[bot]@users.noreply.github.com>

--- terraform_pc/simple_instance/jlopezs3.tf
+++ terraform_pc/simple_instance/jlopezs3.tf
@@ -12,6 +12,54 @@ resource "aws_s3_bucket" "jlopezs3" {
 }
 
 
+resource "aws_s3_bucket_versioning" "jlopezs3" {
+  bucket = aws_s3_bucket.jlopezs3.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket" "destination" {
+  bucket = aws_s3_bucket.jlopezs3.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_iam_role" "replication" {
+  name = "aws-iam-role"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_replication_configuration" "jlopezs3" {
+  depends_on = [aws_s3_bucket_versioning.jlopezs3]
+  role   = aws_iam_role.jlopezs3.arn
+  bucket = aws_s3_bucket.jlopezs3.id
+  rule {
+    id = "foobar"
+    status = "Enabled"
+    destination {
+      bucket        = aws_s3_bucket.destination.arn
+      storage_class = "STANDARD"
+    }
+  }
+}
+
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "jlopezs3" {
   bucket = aws_s3_bucket.jlopezs3.bucket
 
