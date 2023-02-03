99f9c1a65a838155b602e5fa19efbf96449dc9eb
Julian <86843980+Juliandreslopez@users.noreply.github.com>
2023-02-02 20:14:40 -0800

    Update terraform_pc/simple_instance/jlopezs3.tf
    Co-authored-by: prisma-cloud-devsecops[bot] <89982750+prisma-cloud-devsecops[bot]@users.noreply.github.com>

--- terraform_pc/simple_instance/jlopezs3.tf
+++ terraform_pc/simple_instance/jlopezs3.tf
@@ -10,3 +10,14 @@ resource "aws_s3_bucket" "jlopezs3" {
     Environment          = "Dev"
   }
 }
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "jlopezs3" {
+  bucket = aws_s3_bucket.jlopezs3.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
